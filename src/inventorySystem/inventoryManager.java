b88eb63909411109b6f4224c9d19eb8c121f4d9f
Audie Ploe <47163769+AudiePloe@users.noreply.github.com>
2024-08-15 11:54:51 -0500

    Initial commit

---
+++ src/inventorySystem/inventoryManager.java
@@ -0,0 +1,70 @@
+package inventorySystem;
+
+public class inventoryManager {
+	
+	
+	//load all required resources
+			//load main screen
+			
+			//main screen
+				//open projects
+				//open inventory
+				//exit
+			
+			//project screen
+				//list of projects
+					//fetch all project names from folder
+						//open project by name
+				//create new project
+			
+			//project screen
+				//list of all items in project
+				//add new item
+					//list of all available items from inventory list
+					//select to add
+						//input reqs of item to project
+					//save project
+				//save project
+				//build project
+					//print project requirements
+					//remove items from inventory
+			
+			//inventory screen
+				//list of all items created, with quantity
+				//edit item
+					//input new item reqs
+					//add item to inventory
+					//save inventory
+				//add new item
+					//input item reqs
+					//add item to inventory
+					//save inventory
+				//remove item
+					//delete item from inventory
+					//save inventory
+				
+	
+	
+	public static void main(String[] args)
+	{
+		initilize();
+		
+	}
+	
+	private static void initilize()
+	{
+		loadItems();
+		loadProjects();
+	}
+	
+	private static void loadItems()
+	{
+		
+	}
+	
+	private static void loadProjects()
+	{
+		
+	}
+
+}
---
+++ src/module-info.java
@@ -0,0 +1,2 @@
+module pulseMac {
+}
