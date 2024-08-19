f31c7d2790ff4b5e6df3a8abb17588c2d0b59f82
Audie Ploe <47163769+AudiePloe@users.noreply.github.com>
2024-08-19 09:58:46 -0500

    Added correct libraries
    fixed library version and added demo code to test

--- src/inventorySystem/inventoryManager.java
+++ src/inventorySystem/inventoryManager.java
@@ -1,6 +1,18 @@
 package inventorySystem;
 
+
 import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.IOException;
+
+import org.apache.poi.*;
+import org.apache.poi.xssf.usermodel.XSSFCell;
+import org.apache.poi.xssf.usermodel.XSSFCreationHelper;
+import org.apache.poi.xssf.usermodel.XSSFRow;
+import org.apache.poi.xssf.usermodel.XSSFSheet;
+import org.apache.poi.xssf.usermodel.XSSFWorkbook;
+
 
 public class inventoryManager {
 	
@@ -63,6 +75,41 @@ public class inventoryManager {
 	
 	private static void loadItems()
 	{
+		
+		
+		XSSFWorkbook wb = new XSSFWorkbook();
+		XSSFCreationHelper createHelper = wb.getCreationHelper();
+		XSSFSheet sheet = wb.createSheet("new sheet");
+
+		// Create a row and put some cells in it. Rows are 0 based.
+		XSSFRow row = sheet.createRow((short)0);
+		// Create a cell and put a value in it.
+		XSSFCell cell = row.createCell(0);
+		cell.setCellValue(1);
+
+		// Or do it on one line.
+		row.createCell(1).setCellValue(1.2);
+		row.createCell(2).setCellValue(
+				createHelper.createRichTextString("This is a string"));
+		row.createCell(3).setCellValue(true);
+
+		// Write the output to a file
+		FileOutputStream out;
+		try {
+			out = new FileOutputStream("workbook.xls");
+			
+			wb.write(out);
+			out.close();
+			
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		
+		
+		
+		
+		
 		
 	}
 	
